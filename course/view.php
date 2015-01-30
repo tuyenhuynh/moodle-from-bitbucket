53903bf0f79a59bd8e6521adb1a9751254d6c8e7
Vadim Klevtsov <vad23klev@gmail.com>
2015-01-30 21:32:55 +0300

    MDL-13785 course: adding/editing activities without page reloads.
    AJAX editing serves to eliminate page reloads during editing of the
    course. But adding or editing activity (or course) opens new page and
    so, after this is done, all course page must be reloaded (with AJAX
    code) for next add (edit).
    
    New modules resolve problem. Adding/editing activity open in dialog, and
    not require page reload.

--- course/view.php
+++ course/view.php
@@ -41,7 +41,6 @@
     if ($section) {
         $urlparams['section'] = $section;
     }
-
     $PAGE->set_url('/course/view.php', $urlparams); // Defined here to avoid notices on errors etc
 
     // Prevent caching of this page to stop confusion when changing page after making AJAX changes
@@ -289,7 +288,21 @@
     // anything after that point.
     course_view(context_course::instance($course->id), $section);
 
-    // Include course AJAX
+    // Include course AJAX.
     include_course_ajax($course, $modnamesused);
+
+    // Include update activity/resource dialog.
+    $directories = glob($CFG->dirroot . '/mod/*' , GLOB_ONLYDIR);
+    $modules = [];
+    foreach ($directories as $directory) {
+        $PAGE->requires->string_for_js('pluginname', pathinfo($directory)['basename']);
+        $key = pathinfo($directory)['basename'];
+        $modules[$key] = get_string('pluginname', $key);
+    }
+    $PAGE->requires->strings_for_js(array(
+                'expandall',
+                'collapseall',
+                ), 'moodle');
+    $PAGE->requires->yui_module('moodle-course-dialogupdate', 'M.course.dialogupdate().setupDialog', [$modules]);
 
     echo $OUTPUT->footer();
