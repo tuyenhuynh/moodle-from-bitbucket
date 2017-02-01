2bcc5ee1d6090ee3873b116a6fe4f2cbf657b665
Tuyen Huynh <tuyenhm93@gmail.com>
2017-02-01 12:50:34 +0300

    Update moodle-course-dialogupdate-debug.js

--- course/yui/build/moodle-course-dialogupdate/moodle-course-dialogupdate-debug.js
+++ course/yui/build/moodle-course-dialogupdate/moodle-course-dialogupdate-debug.js
@@ -7,6 +7,7 @@ YUI.add('moodle-course-dialogupdate', function (Y, NAME) {
      */
 
     var CSS = {
+        BODY            : 'body', 
         PANELCONTENT    : '#panelContentUpdate',
         PANELHEADER     : "#panelContentUpdate .yui3-widget-hd",
         FORMOPPONENT    : '#mform1',
@@ -19,11 +20,16 @@ YUI.add('moodle-course-dialogupdate', function (Y, NAME) {
         FORM            : 'mform1',
         INITBUTTON      : 'a.editing_update.menu-action.cm-edit-action',
         DUPLICATEBUTTON : 'a.editing_duplicate.menu-action.cm-edit-action',
-        SESSKEY         : "#panelContentUpdate form#mform1 input[name=sesskey]",
-        COURSEID        : "#panelContentUpdate form#mform1 input[name=course]",
-        UPDATE          : "#panelContentUpdate form#mform1 input[name=update]", 
-        MODULENAME      : '#panelContentUpdate form#mform1 input[name=modulename]', 
+        COURSEID        : '#panelContentUpdate form#mform1 input[name=course]',
+        UPDATE          : '#panelContentUpdate form#mform1 input[name=update]', 
+        MODULETYPE      : '#panelContentUpdate form#mform1 input[name=modulename]', 
+        MODULETITLE      : '#panelContentUpdate form#mform1 input[name=name]', 
+        MODULEDESCRIPTION      : '#panelContentUpdate form#mform1 div#id_introeditoreditable', 
+        SHOWDESCRIPTION      : '#panelContentUpdate form#mform1 input[name=showdescription]', 
+        MODULETLABEL      : '#panelContentUpdate form#mform1 input[name=label]', 
+        WIDGETMASK :    '.yui3-widget-mask', 
         SECTION         : '#panelContentUpdate form#mform1 input[name=section]', 
+
     };
 
     var DIALOGNAME = 'course-dialogupdate';
@@ -39,7 +45,6 @@ YUI.add('moodle-course-dialogupdate', function (Y, NAME) {
         DIALOG.superclass.constructor.apply(this, arguments);
     };
     Y.extend(DIALOG, Y.Base, {
-            section: 0, 
             get_query_params: function(url) {
                 var params = {};
                 var url = document.URL;
@@ -68,10 +73,11 @@ YUI.add('moodle-course-dialogupdate', function (Y, NAME) {
              * @param {event} e event object
              */
             submit_click: function (e) {
-                var modulename  = Y.one(CSS.MODULENAME).get("value"); 
-                var validatorName = 'validate_mod_' + modulename + '_mod_form';   
-                if(eval(validatorName)()){
-                    e.preventDefault();
+                e.preventDefault(); 
+                var moduleType  = Y.one(CSS.MODULETYPE).get("value"); 
+                var validatorName = 'validate_mod_' + moduleType + '_mod_form';   
+                if(clientSideValidator()){
+                    
                     // Disable submit buttons to say moodle, what action we do.
                     Y.one(CSS.CANCELBUTTON).set('disabled', 'disabled');
                     if (Y.one(CSS.SUBMITBUTTON) !== null) {
@@ -79,19 +85,33 @@ YUI.add('moodle-course-dialogupdate', function (Y, NAME) {
                     }
 
                     var courseId = Y.one(CSS.COURSEID).get("value");
-                    var sesskey = Y.one(CSS.SESSKEY).get("value");
                     var update = Y.one(CSS.UPDATE).get("value");
+                    var section = Y.one(CSS.SECTION).get("value");
+                    window.section = section;  
                     // AJAX post form request to server side.
                     var cfg = {
                         method: "post",
                         on: {
-                            success: this.post_success
-
+                            success: function(id, o, self) {
+                                var moduleTitle = Y.one(CSS.MODULETITLE);
+                                var moduleDescription = Y.one(CSS.MODULEDESCRIPTION);
+                                var showDescription = Y.one(CSS.SHOWDESCRIPTION);
+                                var moduleLabel = Y.one(CSS.MODULELABEL); 
+                                self.post_success(id, o, self, 
+                                    {
+                                        moduleTitle: moduleTitle, 
+                                        moduleDescription: moduleDescription, 
+                                        showDescription: showDescription, 
+                                        moduleLabel: moduleLabel
+                                    }); 
+                            }
                         },
                         arguments: this,
                         form: { id:CSS.FORM}
-                    };
-                    Y.io(M.cfg.wwwroot + "/course/rest.php?class=updatemod&courseId=" + courseId + "&sesskey=" + sesskey + "&update=" + update, cfg);                    
+                    }; 
+                    Y.io(M.cfg.wwwroot + "/course/rest.php?class=updatemod&courseId=" + courseId  + "&update=" + update, cfg);                    
+                }else {
+                    alert('invalid input'); 
                 }
             },
             /**
@@ -103,47 +123,18 @@ YUI.add('moodle-course-dialogupdate', function (Y, NAME) {
              * @param {object} o response object
              * @param {object} self dialog object
              */
-            post_success : function (id, o, self) {
-                if(o.responseText.indexOf(CSS.FORM) === -1) {
-                    var url = document.URL;     
-                    var query = {};
-                    var a = url.substr(url.search("\\?")+1).split('&');
-                    for (var i in a) {
-                        var b = a[i].split('=');
-                        query[decodeURIComponent(b[0])] = decodeURIComponent(b[1]);
-                    }
-                    var cfg = {
-                        on: {
-                            success: self.update_course
-                        }
-                    };
-                    var id = query.id; 
-                    //remove hash if there's one
-                    if(query.id.indexOf('#') > 0) {
-                       query.id = query.id.split('#')[0];      
-                    }
-                    this.section = Y.one(CSS.SECTION).get('value'); 
-                    Y.io(M.cfg.wwwroot + "/course/view.php?id=" + query.id, cfg);
-                } else {
-                    self.show_form(id,o);
+            post_success : function (id, o, self, data) {
+                var titleNode = selectedNodeContainer.get('previousSibling').get('children').item(0).get('children').item('0');
+                var descriptionNode = selectedNodeContainer.get('previousSibling').get('children').item(0).get('children').item('0').get('children').item('1');
+                if(data.moduleTitle) {
+                    titleNode.set('innerHTML', data.moduleTitle.get('value')); 
                 }
+
+                Y.one(CSS.PANELCONTENT).remove(); 
+                Y.one(CSS.WIDGETMASK).remove(); 
+                window.location.hash = '#section-' + window.section; 
             },
 
-
-            /**
-             * Helper function, update course view, else show form with errors.
-             *
-             * @method update_course
-             * @param {Int} id identification number of current transaction
-             * @param {object} o response object
-             */
-            update_course : function (id, o) {
-                // Update course view
-                document.open();
-                document.write(o.responseText);
-                document.close();
-                window.location.hash = "#section-" + this.section; 
-            },
             /**
              * Show form for setting up and update dialogue for courses.
              *
@@ -151,38 +142,37 @@ YUI.add('moodle-course-dialogupdate', function (Y, NAME) {
              * @param {Int} id identification number of current transaction
              * @param {object} o response object
              */
-            show_form: function (x, o) {
-
-                var responseObject = JSON.parse(o.responseText);
-
-                //Remove old panel if it exist
-                if (Y.one(CSS.PANELCONTENT) !== null) {
-                    Y.one(CSS.PANELCONTENT).remove();
+            show_form: function (html, js, module_type) { 
+                var panelContent = Y.one(CSS.PANELCONTENT); 
+                if (panelContent !== null) {
+                    panelContent.remove();
                 }
                 var opponents = Y.all(CSS.FORMOPPONENT);
                 for (var i = 0; i < opponents.size(); i++) {
                     opponents.item(i).remove();
                 }
-                if (Y.one(CSS.JSVALIDATE) !== null) {
-                    Y.one(CSS.JSVALIDATE).remove();
+                var jsValidate = Y.one(CSS.JSVALIDATE); 
+                if (jsValidate !== null) {
+                    jsValidate.remove();
                     Y.one(CSS.JSFORM).remove();
                 }
                 // Create panel and append it to page
-                var panelContent = Y.Node.create('<div>');
-                panelContent.set('id','panelContentUpdate');
-                Y.one(CSS.COURSEVIEW).appendChild(panelContent);
-                panelContent.setContent(responseObject.html);
-                Y.one(CSS.FORMCONTAINER).setStyle("height","500px");
-                Y.one(CSS.FORMCONTAINER).setStyle("padding-left","30px");
-                Y.one(CSS.FORMCONTAINER).setStyle("padding-right","30px");
-                Y.one(CSS.FORMCONTAINER).setStyle("overflow","auto");
+                var panelContentNode = Y.Node.create('<div>');
+                panelContentNode.set('id','panelContentUpdate');
+                Y.one(CSS.BODY).appendChild(panelContentNode);
 
+                panelContentNode.setContent(html);
+                var formContainer = Y.one(CSS.FORMCONTAINER) ; 
+                formContainer.setStyle("height","500");
+                formContainer.setStyle("padding-left","30px");
+                formContainer.setStyle("padding-right","30px");
+                formContainer.setStyle("overflow","auto");  
                 // Create panel object
-                var panel = new Y.Panel({
+                panel = new Y.Panel({
                     srcNode      : CSS.PANELCONTENT,
-                    headerContent: 'Edit',
+                    headerContent: 'Update ' + module_type,
                     width        : 1000,
-                    zIndex       : 5,
+                    zIndex       : 1035,
                     centered     : true,
                     modal        : true,
                     visible      : false,
@@ -190,32 +180,33 @@ YUI.add('moodle-course-dialogupdate', function (Y, NAME) {
                     plugins      : []
                 });
                 // Move panel down, if it partially visible
-                if (Y.one(CSS.PANELCONTENT).getY() < 50) {
-                    Y.one(CSS.PANELCONTENT).setY(50);
+                panelContent = Y.one(CSS.PANELCONTENT);
+                if (panelContent.getY() < 50) {
+                    panelContent.setY(50);
                 }
-                Y.one(CSS.PANELCONTENT).setStyle("padding-bottom", "10px");
-                Y.one(CSS.PANELCONTENT).setStyle("border-radius", "10px");
-                Y.one(CSS.PANELHEADER).setStyle("border-radius", "10px 10px 0px 0px");
-                
-                //Show panel
+
                 panel.show();
-                // Set reaction on "Cancel"
+                panelContent.setStyle("padding-bottom", "10px");
+                panelContent.setStyle("border-radius", "10px");
+                panelContent.setStyle("top", "0px");
+                Y.one(CSS.PANELHEADER).setStyle("border-radius", "10px 10px 0px 0px");
                 Y.fire('domready');
                 Y.one(CSS.CANCELBUTTON).on('click', function (e1) {
                     e1.preventDefault();
                     panel.hide();
-                });
-                Y.one(CSS.CANCELBUTTON2).on('click', function (e1) {
-                    e1.preventDefault();
-                    panel.hide();
-                });
+                },this);
 
+                //inject validator to windows
+                var moduleType = Y.one(CSS.MODULETYPE).get('value');
+                var validatorName = "validate_mod_" + moduleType + "_mod_form";
+                var formListenerDecl = "document.getElementById('mform1').addEventListener"; 
+                js = 'skipClientValidation=false;' + js;
+                js = js.replace(formListenerDecl, 'window.clientSideValidator=' + validatorName + ';\n' + formListenerDecl);     
+                js+= 'Y.use("moodle-course-dialogupdate", function() {M.course.dialogupdate().change_submit();})';
                 // execute the response script
                 var scriptElement = document.createElement('script');
-                scriptElement.textContent = responseObject.script;
-                document.body.appendChild(scriptElement);
-                console.log(window.validate_mod_assign_mod_form); 
-
+                scriptElement.textContent = js;
+                document.body.appendChild(scriptElement); 
             },
             /**
              * Reaction on click "Save and return to course",
@@ -227,23 +218,33 @@ YUI.add('moodle-course-dialogupdate', function (Y, NAME) {
              */
             on_click: function (e,self) {
                 e.preventDefault();
-                // AJAX request to get information to form
-                var cfg = {
-                    on: {
-                        success: self.show_form
-                    },
-                    arguments: self
-                };
-                var params = {};
+                var obj = {};
                 var courseUrl = document.URL;
-                var a = courseUrl.substr(courseUrl.search("\\?")+1).split('&');
-                for (var i in a) {
-                    var b = a[i].split('=');
-                    params[decodeURIComponent(b[0])] = decodeURIComponent(b[1]);
+                var tokens = courseUrl.substr(courseUrl.indexOf('?')+1).split('&');
+                for (var i in tokens) {
+                    var token = tokens[i].split('=');
+                    obj[decodeURIComponent(token[0])] = decodeURIComponent(token[1]);
                 }
-                var url = this.get('href');
-                Y.io(M.cfg.wwwroot + '/course/rest.php'+
-                    url.substr(url.indexOf('?')) + '&class=updatemod&courseId='+params.id, cfg);
+                var targetUrl = e._currentTarget.href.split('#')[0];
+                tokens = targetUrl.substr(targetUrl.indexOf("?")+1).split('&');
+                for (var i in tokens) {
+                    var token = tokens[i].split('=');
+                    obj[decodeURIComponent(token[0])] = decodeURIComponent(token[1]);
+                }
+                var selectedNodeContainer = Y.one('a.editing_update.menu-action.cm-edit-action[href=' + targetUrl + ']');
+                
+                for(i = 0 ; i < 5 ; ++i) {
+                    selectedNodeContainer = selectedNodeContainer.ancestor(); 
+                } 
+                window.selectedNodeContainer = selectedNodeContainer; 
+                
+                require(['core/fragment'], function(fragment){
+                    var params = { update : obj.update, sr: obj.sr};
+                    fragment.loadFragment('mod_' + obj.module_type, 'update_' + obj.module_type, obj.contextid, params)// return a promise (project of deferred)
+                        .then(function(html, js) {
+                            self.show_form(html, js, obj.module_type); 
+                        });                    
+                });
             },
             /**
              * Set up the activity form.
